cda010496a85d47023e21fd813b2428ab63e45e1
Natalia <natorgom@gmail.com>
2019-03-07 16:49:35 +0100

    Fix minor bugs
    Fix: remove unnecessary addToList
    Fix: add key in map
    Comment: Add comment with review alert
    Co-Authored-By: Julián González <jgp1971@users.noreply.github.com>

--- src/components/searchlist.js
+++ src/components/searchlist.js
@@ -2,7 +2,8 @@ import React from 'react';
 import SearchResult from './searchresult';
 
 const SearchList = ({results}) => {
-  return results.map(result => <SearchResult restaurant={result}/>);
+
+  return results.map((result, index) => <SearchResult key={index} restaurant={result}/>);
 }
 
 export default SearchList;
